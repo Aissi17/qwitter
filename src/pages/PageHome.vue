49c0fa9d9f65e6487844014d4586a08acd594feb
Islam <aissiislam97@gmail.com>
2021-04-24 17:43:54 +0100

    new tweet form on home page

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,11 +1,51 @@
 <template>
   <q-page >
-    <h5>Home</h5>
+    <div class="q-py-lg  q-px-md row items-end q-col-gutter-md">
+
+      <div class="col">
+          <q-input
+          bottom-slots 
+          v-model="newQweet" 
+          placeholder="wha's up?" 
+          counter 
+          maxlength="280" 
+          class="new-qweet"
+          autogrow
+         >
+            <template v-slot:before>
+              <q-avatar>
+                <img src="https://avatarfiles.alphacoders.com/163/thumb-1920-163207.jpg">
+              </q-avatar>
+            </template>
+        </q-input>
+      </div>
+
+      <div class="col col-shrink">
+          <q-btn class="q-mb-lg" :disable="!newQweet" unelevated rounded color="primary" label="Qweet"  no-caps/>
+      </div>
+    </div>
+
+    <q-separator size="10px" color="grey-2" class="divider" />
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageHome'
+  name: 'PageHome',
+  data(){
+    return {
+      newQweet : '',
+    }
+  }
 }
 </script>
+<style lang="sass">
+.new-sweet 
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important 
+.divider
+  border-top : 1px solid
+  border-bottom: 1px solid
+  border-color : $grey-4
+</style>
