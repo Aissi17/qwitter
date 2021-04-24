1c1cf343d8c1f6e2e9a468ce7bdadbb7d6d70078
Islam <aissiislam97@gmail.com>
2021-04-24 16:50:46 +0100

    about page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,16 @@
 <template>
-  <q-page>
-      <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold" >About Qwitter</h4>
+      <div class="text-body1">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.Commodi repellendus dolore aut eligendi nisi voluptatem
+          voluptate dignissimos, ratione cupiditate perspiciatis sit tempore sequi fugit quod vero, ad labore quaerat. Quia!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.Commodi repellendus dolore aut eligendi nisi voluptatem
+          voluptate dignissimos, ratione cupiditate perspiciatis sit tempore sequi fugit quod vero, ad labore quaerat. Quia!
+        </p>
+    </div>
   </q-page>
 </template>
 
