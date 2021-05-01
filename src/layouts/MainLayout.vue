1885c7b441a2749347a357f415071c9c3265652b
Islam <aissiislam97@gmail.com>
2021-05-01 15:09:29 +0100

    bug fixed

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -125,7 +125,11 @@
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      
+      <keep-alive>
+         <router-view />
+      </keep-alive>
+     
     </q-page-container>
 
   </q-layout>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="relative-position" >
-    <q-scroll-area class="absolute fullscreen" >
+    <q-scroll-area class="absolute full-height full-width" >
       <div class="q-py-lg  q-px-md row items-end q-col-gutter-md">
 
         <div class="col">
