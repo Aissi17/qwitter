f1b7a89babfd8352fb8cbcf5b3cf54d7308a7675
Islam <aissiislam97@gmail.com>
2021-06-26 15:00:09 +0100

    updated FIREBASE KEY

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxCdNB9nCQseSSljSFy2WtSMd3MFFRNHM",
-  authDomain: "qwitter-7b946.firebaseapp.com",
-  projectId: "qwitter-7b946",
-  storageBucket: "qwitter-7b946.appspot.com",
-  messagingSenderId: "92589642377",
-  appId: "1:92589642377:web:d49cf05128cdbf6d93ec58"
+  apiKey: "API KEY",
+  authDomain: "YOURDOMAIN.firebaseapp.com",
+  projectId: "ID",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }
 
 firebase.initializeApp(firebaseConfig);
